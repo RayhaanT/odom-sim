2124cf3005b648ca731720557160dd68f5c778e2
RayhaanT <tanweer.rayhaan@gmail.com>
2021-08-22 18:55:18 -0400

    Textures need to be 32-bit depth otherwise segfault

--- src/Main.cpp
+++ src/Main.cpp
@@ -197,7 +197,7 @@ int main()
 	driveShader.use();
 	driveShader.setInt("text", 0);
 	unsigned int driveTexture;
-	loadTexture(driveTexture, "textures/RedTexture.png");
+	loadTexture(driveTexture, "textures/botTexture.bmp");
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, driveTexture);
 
